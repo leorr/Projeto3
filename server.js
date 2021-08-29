40348eb63c7ab60ba3230573d95c33fb55408209
leorr <leorr112624@gmail.com>
2021-08-29 18:51:36 -0300

    test rename

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import userController from "./controller/userController.js";
+import InitiateMongoServer from "./config/db.js";
+
+InitiateMongoServer();
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+
+/**
+ * Router Middleware
+ * Router - /user/*
+ * Method - *
+ */
+app.use("/user", userController);
+
+
+app.listen(PORT, () => {
+  console.log(`Node server started at ${PORT}`);
+});
