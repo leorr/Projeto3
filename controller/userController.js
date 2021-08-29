b37d6a623a47b42b7ed80130db3e503ae972cce5
leorr <leorr112624@gmail.com>
2021-08-29 17:37:11 -0300

    2021/08/29 17:37 login e getUserFromToken ✓

--- controller/userController.js
+++ controller/userController.js
@@ -3,6 +3,8 @@ import { check, validationResult} from "express-validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../model/User.js";
+import auth from "../config/auth.js";
+
 const router = express.Router();
 
 /**
@@ -77,5 +79,80 @@ router.post(
         }
     }
 );
+
+router.post(
+  "/login",
+  [
+    check("password", "Please enter a valid password").isLength({
+      min: 6
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+
+    const { username, password } = req.body;
+    try {
+      let user = await User.findOne({
+        username
+      });
+      if (!user)
+        return res.status(400).json({
+          message: "User Not Exist"
+        });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res.status(400).json({
+          message: "Incorrect Password !"
+        });
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        "randomString",
+        {
+          expiresIn: 3600
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({
+            token
+          });
+        }
+      );
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        message: "Server Error"
+      });
+    }
+  }
+);
+
+/**
+ * @method - GET
+ * @description - Get LoggedIn User
+ * @param - /user/me
+ */
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    res.json(user);
+  } catch (e) {
+    res.send({ message: "Error in Fetching user" });
+  }
+});
 
 export default router;
