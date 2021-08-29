ac94f9e0783f4a4896c096cd32b49db7e6225547
leorr <leorr112624@gmail.com>
2021-08-29 14:31:14 -0300

    2021/08/29 14:31 adiciona funcionalidades do mongo ao node

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const http = require('http');
-let server = http.createServer((req, res) => {
-		console.log(req.headers);
-		console.log(res.getHeaders());
-		res.write('Hello World');
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/projeto3', (err,conn) => {
+
+		if(err) throw err;
+
+		const db = conn.db();
+
+		const collection = db.collection('tests');
+
+		collection.find().toArray(( err,res) => {
+				if(err) throw err;
+				console.log(res)
+				conn.close();
+		});
+
+});
+
+let server = http.createServer((res) => {
+		res.setHeader('Content-type', 'text/html; charset=UTF-8');
 		res.end();
 });
+
 server.listen(8000);
 
