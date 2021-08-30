4808a00d3e87903bcc7acef6b5497a40548f62f6
leorr <leorr112624@gmail.com>
2021-08-29 21:46:15 -0300

    2021/08/29 21:46 undo cors usage

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import userController from "./controller/userController.js";
-import cors from 'cors';
 import InitiateMongoServer from "./config/db.js";
 
 InitiateMongoServer();
@@ -10,7 +9,6 @@ const app = express();
 // PORT
 const PORT = process.env.PORT || 4000;
 
-app.use(cors)
 app.use(express.json());
 
 
