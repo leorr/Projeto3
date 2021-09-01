80006154d30680582019153d0cecb34f09d722b0
leorr <leorr112624@gmail.com>
2021-09-01 20:13:23 -0300

    2021/09/01 20:13 fix reverts and bad feat impls

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userController from "./controller/userController.js";
 import InitiateMongoServer from "./config/db.js";
+import cors from 'cors';
 
 InitiateMongoServer();
 
@@ -9,6 +10,7 @@ const app = express();
 // PORT
 const PORT = process.env.PORT || 4000;
 
+app.use(cors())
 app.use(express.json());
 
 
